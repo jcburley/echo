8a842c00b5d186676ec83344217ec2149e8be617
James Craig Burley <430319+jcburley@users.noreply.github.com>
2020-05-14 20:19:49 -0400

    Support multiple line readers and prompts

--- go/echo.go
+++ go/echo.go
@@ -8,14 +8,16 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 var (
 	helpFlag   bool
-	noReadline bool
+	lineReader string
 	eval       string
 	connectTo  string
 	socket     string
+	prompt     string
 	Stderr     io.Writer = os.Stderr
 	Stdout     io.Writer = os.Stdout
 	Stdin      io.Reader = os.Stdin
@@ -50,14 +52,16 @@ func InitSocket(fn func(func() (string, error), *bufio.Writer)) {
 
 	fmt.Fprintf(conn, "Welcome to echo, client at %s. Close the connection to exit.\n", conn.RemoteAddr())
 
-	if noReadline {
+	switch lineReader {
+	case "":
 		input := bufio.NewReader(conn)
 		fn(func() (string, error) {
+			fmt.Fprint(conn, prompt)
 			return input.ReadString('\n')
 		},
 			bufio.NewWriter(conn))
-	} else {
-		cfg := readline.Config{Prompt: "@ "}
+	case "chzyer/readline":
+		cfg := readline.Config{Prompt: prompt}
 		rl, err := readline.HandleConn(cfg, conn)
 		if err != nil {
 			fmt.Fprintf(Stderr, "Cannot init readline: %v\n", err)
@@ -90,6 +94,11 @@ func echoInput(readFn func() (string, error), writer *bufio.Writer) {
 	}
 }
 
+var lineReaders = map[string]struct{}{
+	"":                struct{}{},
+	"chzyer/readline": struct{}{},
+}
+
 func main() {
 	if eval != "" {
 		fmt.Println(eval)
@@ -99,6 +108,11 @@ func main() {
 	if helpFlag {
 		flag.Usage()
 		ExitEcho(0)
+	}
+
+	if _, found := lineReaders[lineReader]; !found {
+		fmt.Fprintf(Stderr, "Unsupported line reader %s; choose one of: %s\n", lineReader, keys(lineReaders))
+		ExitEcho(4)
 	}
 
 	if connectTo != "" {
@@ -112,14 +126,16 @@ func main() {
 	}
 
 	if socket == "" {
-		if noReadline {
+		switch lineReader {
+		case "":
 			input := bufio.NewReader(Stdin)
 			echoInput(func() (string, error) {
+				fmt.Print(prompt)
 				return input.ReadString('\n')
 			},
 				bufio.NewWriter(Stdout))
-		} else {
-			rl, err := readline.New("> ")
+		case "chzyer/readline":
+			rl, err := readline.New(prompt)
 			if err != nil {
 				fmt.Fprintf(Stderr, "Cannot init readline: %v\n", err)
 				ExitEcho(1)
@@ -141,13 +157,25 @@ func main() {
 	ExitEcho(0)
 }
 
+func keys(m map[string]struct{}) string {
+	var str []string
+	for k, _ := range m {
+		str = append(str, "\""+k+"\"")
+	}
+	return strings.Join(str, ", ")
+}
+
 func init() {
-	flag.BoolVar(&noReadline, "no-readline", false, "Use only canonical line input from stdin")
+	flag.StringVar(&lineReader, "line-reader", "", fmt.Sprintf("Line reader to use, of: %s", keys(lineReaders)))
 	flag.BoolVar(&helpFlag, "h", false, "Print usage info and exit")
 	flag.BoolVar(&helpFlag, "help", false, "Print usage info and exit")
 	flag.StringVar(&eval, "i", "", "input string (instead of reading from stdin)")
 	flag.StringVar(&eval, "input", "", "input string (instead of reading from stdin)")
 	flag.StringVar(&connectTo, "connect-to", "", "act as a client connecting to server at specified address")
 	flag.StringVar(&socket, "socket", "", "socket upon which to listen for connections and then echo their input")
+	flag.StringVar(&prompt, "prompt", "\000", "overrides the default prompt")
 	flag.Parse()
+	if prompt == "\000" {
+		prompt = lineReader + "> "
+	}
 }
